f60382f6486906f521647f6593cb909dcf2939d1
Augusto Wagner Andreoli <andreoliwa@gmail.com>
2019-01-09 14:59:02 -0200

    build: Create but don't push git tag, it is needed by the changelog

--- setup.py
+++ setup.py
@@ -78,9 +78,8 @@ class UploadCommand(Command):
         self.status("Uploading the package to PyPi via Twine…")
         os.system("twine upload dist/*")
 
-        # self.status("Pushing git tags…")
-        # os.system("git tag v{0}".format(about["__version__"]))
-        # os.system("git push --tags")
+        self.status("Creating git tag…")
+        os.system("git tag v{0}".format(about["__version__"]))
 
         sys.exit()
 
