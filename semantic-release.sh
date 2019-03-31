140cc4de487dcc32713b641b91aa9f7cb29c7102
Augusto Wagner Andreoli <andreoliwa@gmail.com>
2019-03-31 23:21:13 +0200

    ci: install using npm -g

--- semantic-release.sh
+++ semantic-release.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # https://github.com/semantic-release/semantic-release/blob/master/docs/recipes/travis.md
-npm install -g @semantic-release/changelog @semantic-release/git @semantic-release/exec
-npx semantic-release -d
+npm install -g semantic-release@15 @semantic-release/changelog @semantic-release/git @semantic-release/exec
+semantic-release -d
