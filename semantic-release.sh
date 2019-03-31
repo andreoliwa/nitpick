d77defbb77e117aebaf7a4df8373e59c3db6973c
Augusto Wagner Andreoli <andreoliwa@gmail.com>
2019-03-31 22:20:29 +0200

    ci: install semantic-release plugins

--- semantic-release.sh
+++ semantic-release.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 # https://github.com/semantic-release/semantic-release/blob/master/docs/recipes/travis.md
-# Use nvm to install and use the Node LTS version (nvm is installed on all Travis images)
-nvm install lts/*
+npm install -g @semantic-release/changelog @semantic-release/git @semantic-release/exec
 npx semantic-release -d
