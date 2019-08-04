d25a0f83b3015a672dab9268437b0d4b9279c791
Augusto Wagner Andreoli <andreoliwa@gmail.com>
2019-08-04 19:17:47 +0200

    build: update dependencies and hooks

--- nitpick/typedefs.py
+++ nitpick/typedefs.py
@@ -1,11 +1,11 @@
 """Type definitions."""
 from pathlib import Path
-from typing import Any, Iterator, List, MutableMapping, Tuple, Type, Union
+from typing import Any, Dict, Iterator, List, Tuple, Type, Union
 
 from ruamel.yaml.comments import CommentedMap, CommentedSeq
 
 PathOrStr = Union[Path, str]
-JsonDict = MutableMapping[str, Any]
+JsonDict = Dict[str, Any]
 StrOrList = Union[str, List[str]]
 Flake8Error = Tuple[int, int, str, Type]
 YieldFlake8Error = Iterator[Flake8Error]
