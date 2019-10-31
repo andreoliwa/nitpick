873b3c837da0fe62e0c1bf41db3f80eb82bf55d3
W. Augusto Andreoli <andreoliwa@gmail.com>
2019-11-01 00:03:02 +0100

    ci: fix deployment error on Travis

---
+++ .travis/deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+pyenv local 3.7
+pip install --upgrade pip poetry pre-commit bumpversion twine
+npm install -g semantic-release@15 @semantic-release/changelog @semantic-release/git @semantic-release/exec
+semantic-release
