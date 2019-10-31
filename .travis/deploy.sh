cd09cca6187ac6bcc0da888effdce21f58d4c94e
Augusto Wagner Andreoli <andreoliwa@gmail.com>
2019-10-31 23:20:10 +0100

    ci: install semantic-release plugins

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Use nvm to install and use the Node LTS version (nvm is installed on all Travis images)
-pip install -U poetry pre-commit bumpversion twine
-# fixme @semantic-release/changelog @semantic-release/git @semantic-release/exec
+pip3 install -U poetry pre-commit bumpversion twine
+npm install -g @semantic-release/changelog @semantic-release/git @semantic-release/exec
 npx semantic-release
