28574b9d8afaaad1773004feb190eba190692a3f
Augusto W. Andreoli <andreoliwa@gmail.com>
2020-09-18 03:16:27 +0200

    ci: debug missing Python

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,14 @@
-#!/usr/bin/env bash
-pyenv local 3.7
-pip install --upgrade pip poetry pre-commit bumpversion twine
+#!/usr/bin/env bash -x
+set -exu
+echo "Installed versions"
+python --version
+python3 --version
+pyenv --version
+pyenv version
+
+echo "Starting deployment"
+pyenv local 3.8
+python3 -m pip install --upgrade pip poetry pre-commit bumpversion twine
 npm install -g semantic-release @semantic-release/changelog \
     @semantic-release/git @semantic-release/exec
 semantic-release
