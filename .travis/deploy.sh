ab82df17186c4616e59881fb0fa051f46eff2d21
Augusto W. Andreoli <andreoliwa@gmail.com>
2020-09-18 03:19:51 +0200

    ci: don't use pyenv, try local Python 3.5

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,7 +7,6 @@ pyenv --version
 pyenv version
 
 echo "Starting deployment"
-pyenv local 3.8
 python3 -m pip install --upgrade pip poetry pre-commit bumpversion twine
 npm install -g semantic-release @semantic-release/changelog \
     @semantic-release/git @semantic-release/exec
