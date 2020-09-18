926b03d48afd6ca0aabdb9d75a35777b9c9e66c0
Augusto W. Andreoli <andreoliwa@gmail.com>
2020-09-18 03:36:47 +0200

    ci: fix Python version on Travis (#210)

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
-pyenv local 3.7
-pip install --upgrade pip poetry pre-commit bumpversion twine
+set -exu
+echo "Installed versions"
+python --version
+python3 --version
+pyenv --version
+pyenv version
+pyenv versions
+pip --version
+
+echo "Starting deployment"
+pyenv global 3.7.1
+pip3 install --upgrade pip poetry pre-commit bumpversion twine
 npm install -g semantic-release @semantic-release/changelog \
     @semantic-release/git @semantic-release/exec
 semantic-release
