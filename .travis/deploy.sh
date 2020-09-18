a1ae61ad4d92b52ce2f88f47bf533984fe35a8d1
Augusto W. Andreoli <andreoliwa@gmail.com>
2020-09-18 03:26:21 +0200

    ci: display pyenv versions

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,10 +5,11 @@ python --version
 python3 --version
 pyenv --version
 pyenv version
+pyenv versions
 pip --version
-pip3 --version
 
 echo "Starting deployment"
+pyenv global 3.7.1
 pip3 install --upgrade pip poetry pre-commit bumpversion twine
 npm install -g semantic-release @semantic-release/changelog \
     @semantic-release/git @semantic-release/exec
