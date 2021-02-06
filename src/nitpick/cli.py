e642069a8f0a8f5e4b8cb247302511adad085013
Augusto W. Andreoli <andreoliwa@gmail.com>
2021-02-06 15:28:46 +0100

    ci: adjust codeclimate, fix some warnings

--- src/nitpick/cli.py
+++ src/nitpick/cli.py
@@ -2,16 +2,15 @@
 
 Why does this file exist, and why not put this in __main__?
 
-  You might be tempted to import things from __main__ later, but that will cause
-  problems: the code will get executed twice:
+You might be tempted to import things from __main__ later, but that will cause
+problems: the code will get executed twice:
 
-  - When you run `python -mnitpick` python will execute
-    ``__main__.py`` as a script. That means there won't be any
-    ``nitpick.__main__`` in ``sys.modules``.
-  - When you import __main__ it will get executed again (as a module) because
-    there's no ``nitpick.__main__`` in ``sys.modules``.
+- When you run `python -mnitpick` python will execute ``__main__.py`` as a script.
+  That means there won't be any ``nitpick.__main__`` in ``sys.modules``.
+- When you import __main__ it will get executed again (as a module) because
+  there's no ``nitpick.__main__`` in ``sys.modules``.
 
-  Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
+Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
 """
 import os
 from enum import Enum
