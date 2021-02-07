45476d2c3916c2eebcc2a6f16c010d6b8f81aca9
Augusto W. Andreoli <andreoliwa@gmail.com>
2021-02-08 00:20:21 +0100

    ci: restore Windows builds on GitHub Actions (#262)
    * refactor: use os.path.sep as path separator
    * ci: mark Windows tests as xfail
    * ci: skip some tox envs on Windows
    * ci: test with ubuntu-20.04 already (will be default soon)
    * ci: restore ubuntu-latest (ubuntu-20.04 worked fine)

--- tests/test_generic.py
+++ tests/test_generic.py
@@ -1,6 +1,9 @@
 """Generic functions tests."""
+import sys
 from pathlib import Path
 from unittest import mock
+
+import pytest
 
 from nitpick.generic import MergeDict, get_subclasses, relative_to_cur_home_abs
 from tests.helpers import assert_conditions
@@ -38,24 +41,32 @@ def test_merge_dicts_extending_lists():
     )
 
 
-@mock.patch.object(Path, "cwd", return_value=Path("/home/john/project"))
-@mock.patch.object(Path, "home", return_value=Path("/home/john"))
+@mock.patch.object(Path, "cwd")
+@mock.patch.object(Path, "home")
+@pytest.mark.xfail(condition=sys.platform == "win32", reason="Different path separator on Windows")
+# TODO: fix Windows tests.
+#  For this one, create a separate test mocking current and home folders on Windows. e.g: C:\\Users\\runneradmin\\
 def test_relative_to_cur_home_full(home, cwd):
-    """Mock the home and current dirs, and test relative paths to them."""
+    """Mock the home and current dirs, and test relative paths to them (testing Linux-only)."""
+    home_dir = "/home/john"
+    project_dir = f"{home_dir}/project"
+
+    home.return_value = Path(home_dir)
+    cwd.return_value = Path(project_dir)
     for path, expected in {
         None: "",
         # Dirs
-        "/home/john/project": "",
-        Path("/home/john/project"): "",
-        "/home/john/another": "~/another",
-        Path("/home/john/bla/bla"): "~/bla/bla",
+        project_dir: "",
+        Path(project_dir): "",
+        f"{home_dir}/another": "~/another",
+        Path(f"{home_dir}/bla/bla"): "~/bla/bla",
         "/usr/bin/some": "/usr/bin/some",
         Path("/usr/bin/awesome"): "/usr/bin/awesome",
         # Files
-        "/home/john/project/tox.ini": "tox.ini",
-        Path("/home/john/project/apps/manage.py"): "apps/manage.py",
-        "/home/john/another/one/bites/the/dust.py": "~/another/one/bites/the/dust.py",
-        Path("/home/john/bla/bla.txt"): "~/bla/bla.txt",
+        f"{project_dir}/tox.ini": "tox.ini",
+        Path(f"{project_dir}/apps/manage.py"): "apps/manage.py",
+        f"{home_dir}/another/one/bites/the/dust.py": "~/another/one/bites/the/dust.py",
+        Path(f"{home_dir}/bla/bla.txt"): "~/bla/bla.txt",
         "/usr/bin/something/wicked/this-way-comes.cfg": "/usr/bin/something/wicked/this-way-comes.cfg",
         Path("/usr/bin/.awesome"): "/usr/bin/.awesome",
     }.items():
