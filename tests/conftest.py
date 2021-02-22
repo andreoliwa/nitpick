7b9102c9945f00d13c17cdc1d4580c8de99f69ce
Augusto W. Andreoli <andreoliwa@gmail.com>
2021-02-22 01:17:14 +0100

    docs: fix comment

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,15 +8,13 @@ import pytest
 def project_with_default_style(tmp_path):
     """Project with the default Nitpick style.
 
-    .. note::
-
-        These imports below have to be here. within the fixture, otherwise they raise an error on tox:
+    These imports below have to be here within the fixture, otherwise they raise an error on tox:
 
         $ tox -e clean,py38,report
         (...)
         Coverage.py warning: No data was collected. (no-data-collected)
-        .tox/py38/lib/python3.8/site-packages/pytest_cov/plugin.py:271: PytestWarning:
-            Failed to generate report: No data to report.
+        .tox/py38/lib/python3.8/site-packages/pytest_cov/plugin.py:271:
+         PytestWarning: Failed to generate report: No data to report.
     """
     from nitpick.constants import NITPICK_STYLE_TOML
     from tests.helpers import ProjectMock
