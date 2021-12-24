257548140f0305b2ceda017280a0881e70a8fda1
W. Augusto Andreoli <andreoliwa@gmail.com>
2021-12-24 03:13:55 +0100

    chore: fix mypy AGAIN (breaking on CI, not locally)

--- src/nitpick/style/fetchers/pypackage.py
+++ src/nitpick/style/fetchers/pypackage.py
@@ -8,7 +8,7 @@ from nitpick.style.fetchers.base import StyleFetcher
 
 try:
     from importlib.abc import Traversable  # type: ignore[attr-defined]
-    from importlib.resources import files
+    from importlib.resources import files  # type: ignore[attr-defined]
 except ImportError:
     from importlib_resources import files
     from importlib_resources.abc import Traversable
