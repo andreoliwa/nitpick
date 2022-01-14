3f129c8324fe866fee8ce5799d4bdef497f13a58
W. Augusto Andreoli <andreoliwa@gmail.com>
2022-01-14 03:47:47 +0100

    ci: run prettier to fix bumpversion files

--- ci-prepare-cmd.sh
+++ ci-prepare-cmd.sh
@@ -4,13 +4,16 @@ set -e
 
 VERSION=$1
 bumpversion --allow-dirty --no-commit --no-tag --new-version $VERSION patch
-cat pyproject.toml
-cat /home/runner/work/nitpick/nitpick/pyproject.toml
+
+# Clean up the files touched by bumpversion
 pre-commit run --all-files end-of-file-fixer
 pre-commit run --all-files trailing-whitespace
+pre-commit run --all-files prettier
+
 rm -rf dist/
 poetry build
 
 # Hide the password
 set +x
+# TODO: ci: use poetry publish instead of twine
 twine upload --verbose --disable-progress-bar --skip-existing --password $TWINE_TEST_PASSWORD -r testpypi dist/*
