7fb0afe20259fe7b3ae3af8699ec57dc3180effc
W. Augusto Andreoli <andreoliwa@gmail.com>
2022-01-15 05:35:07 +0100

    ci: ignore exit code of pre-commit hooks

--- ci-prepare-cmd.sh
+++ ci-prepare-cmd.sh
@@ -6,9 +6,11 @@ VERSION=$1
 bumpversion --allow-dirty --no-commit --no-tag --new-version $VERSION patch
 
 # Clean up the files touched by bumpversion
+set +e  # Allow failure; if files are modified, pre-commit returns an exit code > 0
 pre-commit run --all-files end-of-file-fixer
 pre-commit run --all-files trailing-whitespace
 pre-commit run --all-files prettier
+set -e
 
 rm -rf dist/
 poetry build
