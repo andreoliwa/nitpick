d752adf34ff92d70b3568f152b6c43eada9636f8
W. Augusto Andreoli <andreoliwa@gmail.com>
2022-01-27 03:00:21 +0100

    test: remaining built-in styles

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -1,15 +1,25 @@
 """Resource tests."""
 from pathlib import Path
+from typing import Dict, List
 
 import pytest
 from identify.identify import ALL_TAGS
 
-from nitpick.constants import DOT_NITPICK_TOML, PRE_COMMIT_CONFIG_YAML, PYLINTRC, PYPROJECT_TOML, SETUP_CFG, TOX_INI
+from nitpick.constants import (
+    DOT_NITPICK_TOML,
+    EDITOR_CONFIG,
+    PACKAGE_JSON,
+    PRE_COMMIT_CONFIG_YAML,
+    PYLINTRC,
+    PYPROJECT_TOML,
+    SETUP_CFG,
+    TOX_INI,
+)
 from nitpick.style.fetchers.pypackage import BuiltinStyle, builtin_styles
 from nitpick.violations import Fuss
 from tests.helpers import STYLES_DIR, ProjectMock
 
-FUSS_CODE_MAPPING = {
+BUILTIN_STYLE_CODES = {
     SETUP_CFG: 321,
     PRE_COMMIT_CONFIG_YAML: 361,
     PYPROJECT_TOML: 311,
@@ -17,6 +27,23 @@ FUSS_CODE_MAPPING = {
     ".readthedocs.yml": 361,
     TOX_INI: 321,
     PYLINTRC: 321,
+    ".github/workflows/python.yaml": 361,
+    EDITOR_CONFIG: 321,
+    PACKAGE_JSON: 341,
+}
+BUILTIN_STYLE_EXTRA_VIOLATIONS: Dict[str, List[Fuss]] = {
+    "any/pre-commit-hooks": [
+        Fuss(
+            False,
+            PRE_COMMIT_CONFIG_YAML,
+            103,
+            " should exist: Create the file with the contents below, then run 'pre-commit install'",
+            "",
+        )
+    ],
+    "python/poetry": [
+        Fuss(False, PYPROJECT_TOML, 103, " should exist: Install poetry and run 'poetry init' to create it", "")
+    ],
 }
 
 
@@ -32,11 +59,11 @@ def test_packages_named_after_identify_tags():
         assert init_py[0].is_file()
 
 
-@pytest.mark.parametrize("path", list(builtin_styles())[:13])  # FIXME: test: all styles
-def test_each_builtin_style(tmp_path, datadir, path):
+@pytest.mark.parametrize("builtin_style_path", builtin_styles())
+def test_each_builtin_style(tmp_path, datadir, builtin_style_path):
     """Test each built-in style."""
-    style = BuiltinStyle.from_path(path)
-    fusses = []
+    style = BuiltinStyle.from_path(builtin_style_path)
+    violations = []
     name_contents = []
     for filename in style.files:
         expected_path = datadir / style.path_from_resources_root / filename
@@ -46,9 +73,11 @@ def test_each_builtin_style(tmp_path, datadir, path):
             fixture_path.touch(exist_ok=True)
 
         expected_contents = expected_path.read_text()
-        code = FUSS_CODE_MAPPING[filename]
-        fusses.append(Fuss(True, filename, code, " was not found. Create it with this content:", expected_contents))
+        code = BUILTIN_STYLE_CODES[filename]
+        violations.append(Fuss(True, filename, code, " was not found. Create it with this content:", expected_contents))
         name_contents.extend([filename, expected_contents])
+
+    violations.extend(BUILTIN_STYLE_EXTRA_VIOLATIONS.get(style.path_from_resources_root, []))
 
     project = ProjectMock(tmp_path).save_file(
         DOT_NITPICK_TOML,
@@ -59,12 +88,17 @@ def test_each_builtin_style(tmp_path, datadir, path):
     )
 
     # Run `nitpick fix` twice on the style
-    # First time: it should report violations and create new file(s)
-    project.api_check_then_fix(*fusses)
+    # First time check: it should report violations and create new file(s)
+    project.api_check_then_fix(*violations)
     if style.files:
         project.assert_file_contents(*name_contents)
 
-    # Second time: it should not report any violation and should not change the existing file(s)
+    has_unfixed_violations = any(not fuss.fixed for fuss in violations)
+    if has_unfixed_violations:
+        # If some violations can't be fixed, we can't check for the second time and we must leave
+        return
+
+    # Second time check: it should not report any violation and should not change the existing file(s)
     project.api_check_then_fix()
     if style.files:
         project.assert_file_contents(*name_contents)
