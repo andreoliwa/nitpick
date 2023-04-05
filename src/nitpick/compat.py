209fe9786c51451b8b5888c9df065cad4f04c055
W. Augusto Andreoli <andreoliwa@gmail.com>
2023-04-05 23:51:52 +0200

    test: don't cover compat.py

--- src/nitpick/compat.py
+++ src/nitpick/compat.py
@@ -3,6 +3,6 @@
 try:
     from importlib.abc import Traversable  # type: ignore[attr-defined]
     from importlib.resources import files  # type: ignore[attr-defined]
-except ImportError:
+except ImportError:  # pragma: no cover
     from importlib_resources import files
     from importlib_resources.abc import Traversable
