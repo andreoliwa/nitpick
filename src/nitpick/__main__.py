4fd81c876d95215faf36cad5f6430a883ec5648e
W. Augusto Andreoli <andreoliwa@gmail.com>
2023-04-06 00:18:40 +0200

    test: don't cover __main__.py

--- src/nitpick/__main__.py
+++ src/nitpick/__main__.py
@@ -5,6 +5,7 @@ Why does this file exist, and why __main__? For more info, read:
 - https://www.python.org/dev/peps/pep-0338/
 - https://docs.python.org/3/using/cmdline.html#cmdoption-m
 """
+# pragma: no cover
 from nitpick.cli import nitpick_cli
 
 if __name__ == "__main__":
