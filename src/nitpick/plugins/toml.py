a01d4c79f2fffc700f15c450f653d88bce1d0346
Dany Gielow <dany.gielow@adup-tech.com>
2023-04-26 07:18:44 +0200

    fix: fixed list changes in TOML files

--- src/nitpick/plugins/toml.py
+++ src/nitpick/plugins/toml.py
@@ -2,11 +2,11 @@
 from __future__ import annotations
 
 from itertools import chain
-from typing import TYPE_CHECKING, Iterator
+from typing import TYPE_CHECKING, Iterator, cast
 
 from tomlkit import dumps, parse
 
-from nitpick.blender import BaseDoc, Comparison, TomlDoc, traverse_toml_tree
+from nitpick.blender import Comparison, TomlDoc, traverse_toml_tree
 from nitpick.plugins import hookimpl
 from nitpick.plugins.base import NitpickPlugin
 from nitpick.violations import Fuss, SharedViolations, ViolationEnum
@@ -42,20 +42,34 @@ class TomlPlugin(NitpickPlugin):
 
         document = parse(toml_doc.as_string) if self.autofix else None
         yield from chain(
-            self.report(SharedViolations.DIFFERENT_VALUES, document, comparison.diff),
-            self.report(SharedViolations.MISSING_VALUES, document, comparison.missing),
+            self.report(SharedViolations.DIFFERENT_VALUES, document, cast(TomlDoc, comparison.diff)),
+            self.report(
+                SharedViolations.MISSING_VALUES,
+                document,
+                cast(TomlDoc, comparison.missing),
+                cast(TomlDoc, comparison.replace),
+            ),
         )
         if self.autofix and self.dirty:
             self.file_path.write_text(dumps(document))
 
-    def report(self, violation: ViolationEnum, document: TOMLDocument | None, change: BaseDoc | None):
+    def report(
+        self,
+        violation: ViolationEnum,
+        document: TOMLDocument | None,
+        change: TomlDoc | None,
+        replacement: TomlDoc | None = None,
+    ):
         """Report a violation while optionally modifying the TOML document."""
-        if not change:
+        if not (change or replacement):
             return
-        if document:
-            traverse_toml_tree(document, change.as_object)
+        if self.autofix:
+            real_change = cast(TomlDoc, replacement or change)
+            traverse_toml_tree(document, real_change.as_object)
             self.dirty = True
-        yield self.reporter.make_fuss(violation, change.reformatted.strip(), prefix="", fixed=self.autofix)
+
+        to_display = cast(TomlDoc, change or replacement)
+        yield self.reporter.make_fuss(violation, to_display.reformatted.strip(), prefix="", fixed=self.autofix)
 
     @property
     def initial_contents(self) -> str:
