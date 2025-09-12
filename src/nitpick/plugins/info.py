b7d4d882088fbaf8bec12793b6bfa94048df8596
Hmvp <hmvp@users.noreply.github.com>
2025-09-12 12:08:02 +0200

    feat: Better file detection to allow more files with text plugin

--- src/nitpick/plugins/info.py
+++ src/nitpick/plugins/info.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass, field
+from pathlib import Path
 from typing import TYPE_CHECKING
 
 from identify import identify
@@ -29,5 +30,11 @@ class FileInfo:
             clean_path = DOT + path_from_root
         else:
             clean_path = DOT + path_from_root[1:] if path_from_root.startswith("-") else path_from_root
-        tags = set(identify.tags_from_filename(clean_path))
+
+        # When we run identify on the actual file we get better tags
+        if Path(clean_path).exists():
+            tags = set(identify.tags_from_path(clean_path))
+        else:
+            tags = set(identify.tags_from_filename(clean_path))
+
         return cls(project, clean_path, tags)
